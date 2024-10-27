e08cdd0e7073946df960fc30f72b5a7295c8ce71
guille <guille@flowtheapp.com>
2024-10-27 23:06:01 +0000

    empezando a entender

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import PdfCard from "./PdfCard";
 
 function App() {
-  const cards = {  maxWidth: "1200px", margin: "0 auto", display: "grid", gap: "1rem", padding : '20px', gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))"}
+  const cards = {
+    maxWidth: "1200px",
+    margin: "0 auto",
+    display: "grid",
+    gap: "1rem",
+    padding: "20px",
+    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+  };
   return (
     <div>
-      <h2 style={{textAlign:'center'}}>List of invoices</h2>
+      <h2 style={{ textAlign: "center" }}>SAMPLE</h2>
       <div style={cards}>
-        <PdfCard title="Oasic ltd Invoice"/>
-        <PdfCard title="Libra ltd Invoice"/>
-        <PdfCard title="Xpress ltd Invoice"/>
-        <PdfCard title="Cardic ltd Invoice"/>
+        <PdfCard title="Test pdf" />
       </div>
     </div>
   );
--- src/Invoice.js
+++ src/Invoice.js
@@ -1,190 +1,232 @@
-import React, { Fragment } from 'react'
-import { Image, Text, View, Page, Document, StyleSheet } from '@react-pdf/renderer';
-import logo from './Logo.png'
+import React, { Fragment } from "react";
+import {
+  Image,
+  Text,
+  View,
+  Page,
+  Document,
+  StyleSheet,
+} from "@react-pdf/renderer";
+import logo from "./Logo.png";
 
-  const Invoice = () => {
+const Invoice = () => {
+  const contract_info = {
+    songtitle: "cancion",
+    city: "city",
+    country: "country",
+    authors: [
+      {
+        full_name: "Guillermo Perez Martín",
+        id: "923649623",
+        address: "Calle de la Constitución, 1",
+        email: "guillermo.perez@gmail.com",
+        ipi: "923649623",
+        managing_entity: "entity",
+        editor: "editor",
+        composition_percent: 50,
+        lyrics_percent: 50,
+      },
+      {
+        full_name: "Dummy Person",
+        id: "123456789",
+        address: "123 Dummy Street",
+        email: "dummy.person@example.com",
+        ipi: "123456789",
+        managing_entity: "dummy_entity",
+        editor: "dummy_editor",
+        composition_percent: 50,
+        lyrics_percent: 50,
+      },
+    ],
+  };
 
-    const reciept_data = {
-        "id": "642be0b4bbe5d71a5341dfb1",
-        "invoice_no": "20200669",
-        "address": "739 Porter Avenue, Cade, Missouri, 1134",
-        "date": "24-09-2019",
-        "items": [
-          {
-            "id": 1,
-            "desc": "do ex anim quis velit excepteur non",
-            "qty": 8,
-            "price": 179.25
-          },
-          {
-            "id": 2,
-            "desc": "incididunt cillum fugiat aliqua Lorem sit Lorem",
-            "qty": 9,
-            "price": 107.78
-          },
-          {
-            "id": 3,
-            "desc": "quis Lorem ad laboris proident aliqua laborum",
-            "qty": 4,
-            "price": 181.62
-          },
-          {
-            "id": 4,
-            "desc": "exercitation non do eu ea ullamco cillum",
-            "qty": 4,
-            "price": 604.55
-          },
-          {
-            "id": 5,
-            "desc": "ea nisi non excepteur irure Lorem voluptate",
-            "qty": 6,
-            "price": 687.08
-          }
-        ]
-      }
+  const styles = StyleSheet.create({
+    page: {
+      fontSize: 11,
+      paddingTop: 20,
+      paddingLeft: 40,
+      paddingRight: 40,
+      lineHeight: 1.5,
+      flexDirection: "column",
+    },
 
-    const styles = StyleSheet.create({
-        page: {fontSize: 11,paddingTop: 20,paddingLeft: 40,paddingRight: 40,lineHeight: 1.5,flexDirection: 'column' },
+    spaceBetween: {
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      color: "#3E3E3E",
+    },
 
-        spaceBetween : {flex : 1,flexDirection: 'row',alignItems:'center',justifyContent:'space-between',color: "#3E3E3E" },
+    titleContainer: { flexDirection: "row", marginTop: 24 },
 
-        titleContainer: {flexDirection: 'row',marginTop: 24},
-        
-        logo: { width: 90 },
+    logo: { width: 90 },
 
-        reportTitle: {  fontSize: 16,  textAlign: 'center' },
+    reportTitle: { fontSize: 16, textAlign: "center" },
 
-        addressTitle : {fontSize: 11,fontStyle: 'bold'}, 
-        
-        invoice : {fontWeight: 'bold',fontSize: 20},
-        
-        invoiceNumber : {fontSize: 11,fontWeight: 'bold'}, 
-        
-        address : { fontWeight: 400, fontSize: 10},
-        
-        theader : {marginTop : 20,fontSize : 10,fontStyle: 'bold',paddingTop: 4 ,paddingLeft: 7 ,flex:1,height:20,backgroundColor : '#DEDEDE',borderColor : 'whitesmoke',borderRightWidth:1,borderBottomWidth:1},
+    addressTitle: { fontSize: 11, fontStyle: "bold" },
 
-        theader2 : { flex:2, borderRightWidth:0, borderBottomWidth:1},
+    invoice: { fontWeight: "bold", fontSize: 20 },
 
-        tbody:{ fontSize : 9, paddingTop: 4 , paddingLeft: 7 , flex:1, borderColor : 'whitesmoke', borderRightWidth:1, borderBottomWidth:1},
+    invoiceNumber: { fontSize: 11, fontWeight: "bold" },
 
-        total:{ fontSize : 9, paddingTop: 4 , paddingLeft: 7 , flex:1.5, borderColor : 'whitesmoke', borderBottomWidth:1},
+    address: { fontWeight: 400, fontSize: 10 },
 
-        tbody2:{ flex:2, borderRightWidth:1, }
-        
-    });
+    theader: {
+      marginTop: 20,
+      fontSize: 10,
+      fontStyle: "bold",
+      paddingTop: 4,
+      paddingLeft: 7,
+      flex: 1,
+      height: 20,
+      backgroundColor: "#DEDEDE",
+      borderColor: "whitesmoke",
+      borderRightWidth: 1,
+      borderBottomWidth: 1,
+    },
 
-    const InvoiceTitle = () => (
-        <View style={styles.titleContainer}>
-            <View style={styles.spaceBetween}>
-                <Image style={styles.logo} src={logo} />
-                <Text style={styles.reportTitle}>Xpress Enterprises</Text>
-            </View>
+    theader2: { flex: 2, borderRightWidth: 0, borderBottomWidth: 1 },
+
+    tbody: {
+      fontSize: 9,
+      paddingTop: 4,
+      paddingLeft: 7,
+      flex: 1,
+      borderColor: "whitesmoke",
+      borderRightWidth: 1,
+      borderBottomWidth: 1,
+    },
+
+    total: {
+      fontSize: 9,
+      paddingTop: 4,
+      paddingLeft: 7,
+      flex: 1.5,
+      borderColor: "whitesmoke",
+      borderBottomWidth: 1,
+    },
+
+    tbody2: { flex: 2, borderRightWidth: 1 },
+  });
+
+  const InvoiceTitle = () => (
+    <View style={styles.titleContainer}>
+      <View style={styles.spaceBetween}>
+        <Image style={styles.logo} src={logo} />
+        <Text style={styles.reportTitle}>Documento</Text>
+      </View>
+    </View>
+  );
+
+  const TableHead = () => (
+    <View style={{ width: "100%", flexDirection: "row", marginTop: 10 }}>
+      <View style={[styles.theader, styles.theader2]}>
+        <Text>Nombre completo</Text>
+      </View>
+      <View style={styles.theader}>
+        <Text>IPI</Text>
+      </View>
+      <View style={styles.theader}>
+        <Text>Entidad de Gestión</Text>
+      </View>
+      <View style={styles.theader}>
+        <Text>Editor</Text>
+      </View>
+      <View style={styles.theader}>
+        <Text>Porcentaje de composición</Text>
+      </View>
+      <View style={styles.theader}>
+        <Text>Porcentaje de letra</Text>
+      </View>
+    </View>
+  );
+
+  const TableBody = () =>
+    contract_info.authors.map((author) => (
+      <Fragment key={author.id}>
+        <View style={{ width: "100%", flexDirection: "row" }}>
+          <View style={[styles.tbody, styles.tbody2]}>
+            <Text>{author.full_name}</Text>
+          </View>
+          <View style={styles.tbody}>
+            <Text>{author.ipi} </Text>
+          </View>
+          <View style={styles.tbody}>
+            <Text>{author.managing_entity}</Text>
+          </View>
+          <View style={styles.tbody}>
+            <Text>{author.editor}</Text>
+          </View>
+          <View style={styles.tbody}>
+            <Text>{author.composition_percent.toFixed(2)}</Text>
+          </View>
+          <View style={styles.tbody}>
+            <Text>{author.lyrics_percent.toFixed(2)}</Text>
+          </View>
         </View>
-    );
+      </Fragment>
+    ));
 
-    const Address = () => (
-        <View style={styles.titleContainer}>
-            <View style={styles.spaceBetween}>
-                <View>
-                    <Text style={styles.invoice}>Invoice </Text>
-                    <Text style={styles.invoiceNumber}>Invoice number: {reciept_data.invoice_no} </Text>
-                </View>
-                <View>
-                    <Text style={styles.addressTitle}>7, Ademola Odede, </Text>
-                    <Text style={styles.addressTitle}>Ikeja,</Text>
-                    <Text style={styles.addressTitle}>Lagos, Nigeria.</Text>
-                </View>
-            </View>
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <InvoiceTitle />
+
+        <View
+          style={{
+            fontSize: 18,
+            marginTop: 10,
+            textAlign: "center",
+          }}
+        >
+          <Text>Reunidos</Text>
         </View>
-    );
 
-    const UserAddress = () => (
-        <View style={styles.titleContainer}>
-            <View style={styles.spaceBetween}>
-                <View style={{maxWidth : 200}}>
-                    <Text style={styles.addressTitle}>Bill to </Text>
-                    <Text style={styles.address}>
-                        {reciept_data.address}
-                    </Text>
-                </View>
-                <Text style={styles.addressTitle}>{reciept_data.date}</Text>
-            </View>
+        <View>
+          <Text>De otra parte</Text>
+          {contract_info.authors.map((author) => (
+            <Text>
+              {author.full_name} con número de identificación nº
+              {author.id}, domicilio en {author.address}y correo electrónico a
+              efectos de notificaciones {author.email}y número IPI {author.ipi}.
+            </Text>
+          ))}
+          <Text>En Adelante "Autores"</Text>
         </View>
-    );
 
-
-    const TableHead = () => (
-        <View style={{ width:'100%', flexDirection :'row', marginTop:10}}>
-            <View style={[styles.theader, styles.theader2]}>
-                <Text >Items</Text>   
-            </View>
-            <View style={styles.theader}>
-                <Text>Price</Text>   
-            </View>
-            <View style={styles.theader}>
-                <Text>Qty</Text>   
-            </View>
-            <View style={styles.theader}>
-                <Text>Amount</Text>   
-            </View>
+        <View
+          style={{
+            fontSize: 16,
+            textAlign: "center",
+          }}
+        >
+          <Text>EXPONEN</Text>
         </View>
-    );
 
-
-    const TableBody = () => (
-       reciept_data.items.map((receipt)=>(
-        <Fragment key={receipt.id}>
-            <View style={{ width:'100%', flexDirection :'row'}}>
-                <View style={[styles.tbody, styles.tbody2]}>
-                    <Text >{receipt.desc}</Text>   
-                </View>
-                <View style={styles.tbody}>
-                    <Text>{receipt.price} </Text>   
-                </View>
-                <View style={styles.tbody}>
-                    <Text>{receipt.qty}</Text>   
-                </View>
-                <View style={styles.tbody}>
-                    <Text>{(receipt.price * receipt.qty).toFixed(2)}</Text>   
-                </View>
-            </View>
-        </Fragment>
-       ))
-    );
-
-    const TableTotal = () => (
-        <View style={{ width:'100%', flexDirection :'row'}}>
-            <View style={styles.total}>
-                <Text></Text>   
-            </View>
-            <View style={styles.total}>
-                <Text> </Text>   
-            </View>
-            <View style={styles.tbody}>
-                <Text>Total</Text>   
-            </View>
-            <View style={styles.tbody}>
-                <Text>
-                    {reciept_data.items.reduce((sum, item)=> sum + (item.price * item.qty), 0)}
-                </Text>  
-            </View>
+        <View style={{ flexDirection: "column", width: 400 }}>
+          <View style={{ flexDirection: "row", marginBottom: 4 }}>
+            <Text style={{ marginHorizontal: 8 }}>•</Text>
+            <Text>
+              Que AUTORES han participado conjuntamente en la creación de una
+              obra musical titulada {contract_info.songtitle}, en adelante " La
+              obra musical y/o la obra".
+            </Text>
+          </View>
+          <View style={{ flexDirection: "row", marginBottom: 4 }}>
+            <Text style={{ marginHorizontal: 8 }}>•</Text>
+            <Text>
+              Que AUTORES han participado conjuntamente en la creación de una
+              obra musical titulada {contract_info.songtitle}, en adelante " La
+              obra musical y/o la obra".
+            </Text>
+          </View>
         </View>
-    );
+        <TableHead />
+        <TableBody />
+      </Page>
+    </Document>
+  );
+};
 
-    return (
-        <Document>
-            <Page size="A4" style={styles.page}>
-                <InvoiceTitle  />
-                <Address/>
-                <UserAddress/>
-                <TableHead/>
-                <TableBody/>
-                <TableTotal/>
-            </Page>
-        </Document>
-          
-    )
-}
-
-export default Invoice
+export default Invoice;
